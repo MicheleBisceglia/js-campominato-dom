024c36f865e2c1376305b4e628bd0b16d172d835
Michele Bisceglia <mikael_bisceglia@libero.it>
2022-04-26 16:50:57 +0200

    Scaffolding

---
+++ js/script.js
@@ -0,0 +1,38 @@
+
+//gestisco il bottone start
+document.getElementById("start").addEventListener("click", Game);
+const startBtn = document.getElementById("start-btn");
+
+// MAIN FUNCTION
+function Game() {
+    // Genero numeri da 1 a 100
+    const title = document.getElementById("title");
+    //Creo un grid item per ogni numero 
+    const grid = document.querySelector(".grid");
+    title.classList.add("hidden");
+    grid.classList.remove("hidden");
+    grid.innerHTML = "";
+    //Genero i numeri da 1 a 100 con ciclo for
+    for (let i = 1; i <= 100 ; i++){
+    const newItem =generatorGridItem(i)
+    newItem.addEventListener("click" , handleCellClick);
+    grid.append(newItem)
+    
+
+   }
+   //assegno le classi al click del grid item
+    function handleCellClick() {
+        this.classList.add("active")
+    }
+
+}
+
+function generatorGridItem(gridNumber){
+
+const gridItem = document.createElement("div");
+gridItem.classList.add("grid-item");
+gridItem.innerHTML = `<span>${gridNumber}</span>`
+
+return gridItem
+}
+
